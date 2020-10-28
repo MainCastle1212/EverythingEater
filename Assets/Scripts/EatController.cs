91a7dad4a3b66d11a255a6c608053edaa2f8e25f
Siki02 <56345423+Siki02@users.noreply.github.com>
2020-10-28 20:09:12 +0900

    食べられたとき大きくする処理

---
+++ Assets/Scripts/EatController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatController : MonoBehaviour
+{
+    private Vector3 PlayerSize;
+    void Start()
+    {
+        PlayerSize = transform.localScale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var eatObj = collision.gameObject;
+        Debug.Log(eatObj.name);
+        //TODO 自分より小さいならばのif文
+        var e = eatObj.GetComponent<EatableObjContoroller>();
+        if (e == null) return;
+        e.Eaten();
+    }
+}
---
+++ Assets/Scripts/EatableObjContoroller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class EatableObjContoroller : MonoBehaviour //,IEatable
+{
+    [SerializeField]
+    private SpriteRenderer Player;
+    [SerializeField]
+    private float Ratio = 10;
+    private float PlayerSize;
+    private SpriteRenderer Renderer;
+    private float ObjSize;
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+        ObjSize = Renderer.bounds.size.x * Renderer.bounds.size.y;
+        Debug.Log(ObjSize);
+    }
+    public void Eaten()
+    {
+        if (!IsEatable()) return;
+
+        var playerScale = Player.transform.localScale;
+        playerScale += Vector3.one * (ObjSize / Ratio);
+
+        Player.transform.localScale = playerScale;
+
+        Destroy(gameObject);
+    }
+    private bool IsEatable()
+    {
+        PlayerSize = Player.bounds.size.x * Player.bounds.size.y;
+        if (PlayerSize >= ObjSize)
+        {
+            Debug.Log("食べられるよ！");
+            return true;
+        }
+        Debug.Log("食べられないよ！");
+        return false;
+    }
+}
