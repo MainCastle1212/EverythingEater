60dce3560d82a47287d3e0f820113dacd2e20345
Siki02 <56345423+Siki02@users.noreply.github.com>
2020-10-30 20:25:34 +0900

    時計式のタイマー導入

---
+++ Assets/UITimerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimerControl : MonoBehaviour
+{
+    [SerializeField]
+    Timer timer;
+    [SerializeField]
+    Image timerSprite;
+
+    private void Start()
+    {
+        StartCoroutine(ClockTimer());
+    }
+    IEnumerator ClockTimer()
+    {
+        while (!timer.IsTimeOver)
+        {
+            var ratio = 1 - timer.Time / timer.StartTime;
+            Debug.Log(ratio);
+            timerSprite.fillAmount = ratio;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
