b95bd1f68a9f5eb02100e8ad78e921225a6ade26
202004g07 <66996129+202004g07@users.noreply.github.com>
2020-11-04 20:28:55 +0900

    #6 分裂・合体機能のためのスクリプト書き換え (#12)
    * 食べる処理スクリプト作成、UIスクリプト書き換え
    
    * UISize書き換えを最大きいプレイヤーに合わせて行う
    
    * 最も大きいプレイヤーか判定用クラスの作成
    
    * #10 一応の解決
    
    * DOTWeen導入＋カメラ制御をCinemachineTargetGroupで行うように
    
    * 名称変更、PlayerPrefabに変更を反映
    
    * 使用していない部分の削除
    
    * #6 分裂・相互捕食
    
    Co-authored-by: Siki02 <56345423+Siki02@users.noreply.github.com>

--- Assets/Scripts/Eats/EatController.cs
+++ Assets/Scripts/Eats/EatController.cs
@@ -6,7 +6,6 @@ public class EatController : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if (!collision.gameObject.CompareTag("Eatable")) return;
 
         var eatObj = collision.gameObject.GetComponent<EatableObjContoroller>();
         if (eatObj == null) return;
