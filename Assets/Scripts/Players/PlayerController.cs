58c420bd3fd55133ee26b48c33c4806c6545ca26
Siki02 <56345423+Siki02@users.noreply.github.com>
2020-11-10 17:07:12 +0900

    移動可能範囲の設定

--- Assets/Scripts/Players/PlayerController.cs
+++ Assets/Scripts/Players/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,30 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     EatableObjSO PlayerSO;
+    [SerializeField]
+    CinemachineTargetGroup targetGroup;
+    [SerializeField]
+    float DistancePlayerScale;
+    [SerializeField]
+    float Distance;
 
+    private float BaffaDistancePlayerScale;
+    private void Start()
+    {
+        BaffaDistancePlayerScale = DistancePlayerScale;
+    }
     void Update()
     {
         PlayerSO.Size = Player.BiggestSize;
+        AwayCamera();
+    }
+    public void AwayCamera()
+    {
+        var targets = targetGroup.m_Targets;
+        if (Player.BiggestSize > DistancePlayerScale)
+        {
+            targets[0].radius += Distance;
+            DistancePlayerScale += BaffaDistancePlayerScale;
+        }
     }
 }
