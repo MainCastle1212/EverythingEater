4c013724f7c5aea46c8f62054d2cbfeec49639e2
Siki02 <56345423+Siki02@users.noreply.github.com>
2020-11-10 16:25:31 +0900

    ObjSizeを場合に合わせて変更

--- Assets/EatableChar.cs
+++ Assets/EatableChar.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class EatableChar : EatableObjContoroller
 {
-    protected override void DestroyGameObjected()
+    BoxCollider2D m_Collider;
+    protected override void Start()
+    {
+        m_Collider = GetComponent<BoxCollider2D>();
+    }
+    public override float ObjSize => m_Collider.bounds.size.x * m_Collider.bounds.size.y;
+    protected override void DestroyedGameObject()
     {
         var parentText = transform.parent.gameObject;
         Destroy(parentText);
 
-        base.DestroyGameObjected();
+        base.DestroyedGameObject();
     }
 }
